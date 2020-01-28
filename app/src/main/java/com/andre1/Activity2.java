630377c3b26cd02c2a5a3f55bf400c6b4f95ea3e
andre <and4rean@inbox.lv>
2020-01-28 23:34:52 +0200

    Initial commit

---
+++ app/src/main/java/com/andre1/Activity2.java
@@ -0,0 +1,46 @@
+package com.andre1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Activity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_main_new);
+    }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Button forgotPassword = findViewById(R.id.btn_forgot_password);
+
+        forgotPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivity();
+            }
+        });
+
+    }
+
+
+
+
+
+    public void openActivity() {
+
+        Intent intent1 = new Intent(this, MainActivity.class);
+        startActivity(intent1);
+    }
+
+
+
+
+}
